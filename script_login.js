47df2d0e62684886c971cad688d04f3b0f7cac43
MantaroKun <caiovictor182@yahoo.com.br>
2023-06-01 00:23:15 -0300

    :rocket:

---
+++ script_login.js
@@ -0,0 +1,28 @@
+
+    
+    var login = "login"
+
+    var senha = "senha";
+    
+    function botaoSubmit(){
+        var logininput= document.getElementById("login").value;
+        var senhainput= document.getElementById("senha").value;
+        var isvalid = validateLogin(logininput, senhainput)
+       if (isvalid == true){
+        window.location.href = "../semaforo.html"
+       }
+       else{
+        window.alert("deu errado")
+       }
+        
+    
+    }
+    function validateLogin(text1, text2){
+       if(text1 != login){
+            return false 
+       }  
+       if(text2 != senha){
+            return false
+       }
+       return true
+    }
