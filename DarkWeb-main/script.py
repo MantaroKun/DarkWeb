eb29f026d9692be8b4e33eceffacbf1ea0a1b9a1
MantaroKun <caiovictor182@yahoo.com.br>
2023-06-14 00:06:49 -0300

    integração back/front QUASE pronta

--- DarkWeb-main/script.py
+++ DarkWeb-main/script.py
@@ -1,7 +1,6 @@
 
 import http.server
 import socketserver
-import urllib.parse
 import json
 
 PORT = 8000
@@ -9,36 +8,40 @@ PORT = 8000
 data_list = list()
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
-    # ...
+    
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "X-Requested-With, Content-type")
+        self.end_headers()
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
+        post_data = json.loads(post_data)
+        print("Received POST data:")
+        data_list.append([post_data])
+        file = open('sad.json', 'w')
+        string = json.dumps(data_list, indent=4)
+        file.write(string)
+        print(string)
+        self._set_headers()
+        self.wfile.write(json.dumps(data_list).encode())
 
-        # Parsing POST data
-        post_data = urllib.parse.parse_qs(post_data.decode())
-
-        # Extração do campo "nome"
-        nome = post_data['nome'][0]
-
-        # Criar a resposta com o nome
-        response_data = {'nome': nome}
-
-        # Adicionar a resposta à lista de dados
-        data_list.append(response_data)
-
-        # Responder com os dados em formato JSON
-        self.send_response(200)
-        self.send_header('Content-Type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps(response_data).encode())
-
+    def do_OPTIONS(self):
+        self._set_headers()
 
     def do_GET(self):
-            self.send_response(200)
+        if self.path == '/data':
+            self._set_headers()
+            self.wfile.write(json.dumps(data_list).encode())
+        else:
+            self.send_response(404)
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
-            self.wfile.write(json.dumps(data_list).encode())
+            self.wfile.write(json.dumps({'error': 'Invalid route'}).encode())
 
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
     print("serving at port", PORT)
